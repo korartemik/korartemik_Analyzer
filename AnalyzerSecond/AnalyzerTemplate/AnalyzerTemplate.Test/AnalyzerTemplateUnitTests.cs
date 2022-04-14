a84b1f0968887e3dd932a2fd9366ba82c3309a58
Artem Korchagin <kor.artemik@yandex.ru>
2022-04-15 00:32:59 +0300

    fitcc: task 1

---
+++ AnalyzerSecond/AnalyzerTemplate/AnalyzerTemplate.Test/AnalyzerTemplateUnitTests.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VerifyCS = AnalyzerTemplate.Test.CSharpCodeFixVerifier<
+    AnalyzerTemplate.AnalyzerTemplateAnalyzer,
+    AnalyzerTemplate.AnalyzerTemplateCodeFixProvider>;
+
+namespace AnalyzerTemplate.Test
+{
+    [TestClass]
+    public class AnalyzerTemplateUnitTest
+    {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestEmpty()
+        {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestNormNaming()
+        {
+            var test = @"using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool a = true;
+    }
+}
+";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task TestDeter()
+        {
+            await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        [|bool notAva = false;|]
+    }
+}
+", @"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool ava = !false;
+    }
+}
+");
+        }
+        [TestMethod]
+        public async Task TestDefen()
+        {
+            await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        [|bool notAva = false;|]
+        notAva = false;
+    }
+}
+", @"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool ava = !false;
+        ava = !false;
+    }
+}
+");
+        }
+        [TestMethod]
+        public async Task TestDeterInOtherArg()
+        {
+            await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        [|bool notAva = false;|]
+        bool r = notAva;
+    }
+}
+", @"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool ava = !false;
+        bool r = !ava;
+    }
+}
+");
+        }
+        [TestMethod]
+        public async Task TestUnianLogicalExpression()
+        {
+            await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        [|bool notAva = false;|]
+        if(notAva){Console.WriteLine();}
+    }
+}
+", @"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool ava = !false;
+        if(!ava){Console.WriteLine();}
+    }
+}
+");
+        }
+        [TestMethod]
+        public async Task TestLeftLogicalExpression()
+        {
+            await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        [|bool notAva = false;|]
+        if(notAva&&true){Console.WriteLine();}
+    }
+}
+", @"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool ava = !false;
+        if(!ava &&true){Console.WriteLine();}
+    }
+}
+");
+        }
+        [TestMethod]
+        public async Task TestRightLogicalExpression()
+        {
+            await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        [|bool notAva = false;|]
+        if(true&&notAva){Console.WriteLine();}
+    }
+}
+", @"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool ava = !false;
+        if(true&& !ava){Console.WriteLine();}
+    }
+}
+");
+        }
+        [TestMethod]
+        public async Task TestDeterWithoutDefen()
+        {
+            await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        [|bool notAva;|]
+    }
+}
+", @"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool ava;
+    }
+}
+");
+        }
+        [TestMethod]
+        public async Task TestManyDeterWithoutDefen()
+        {
+            await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        [|bool notAva, anonc;|]
+    }
+}
+", @"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool ava, anonc;
+    }
+}
+");
+        }
+        [TestMethod]
+        public async Task TestArgInMethod()
+        {
+            await VerifyCS.VerifyCodeFixAsync(@"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        [|bool notAva = false;|]
+        Console.WriteLine(notAva);
+    }
+}
+", @"
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool ava = !false;
+        Console.WriteLine(!ava);
+    }
+}
+");
+        }
+    }
+}
---
+++ AnalyzerSecond/AnalyzerTemplate/AnalyzerTemplate/AnalyzerTemplateAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace AnalyzerTemplate
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AnalyzerTemplateAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "AnalyzerTemplate";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.LocalDeclarationStatement);
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            
+            var localDeclaration = (LocalDeclarationStatementSyntax)context.Node;
+            if (localDeclaration.Declaration.Type.GetText().ToString().ToLower().Contains("bool") && (localDeclaration.Declaration.Variables.Any(g => g.Identifier.ValueText.Length > 3 && g.Identifier.ValueText.StartsWith("not"))))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation(), localDeclaration.Declaration.Variables.First().Identifier.ValueText));
+            }
+        }
+    }
+}
